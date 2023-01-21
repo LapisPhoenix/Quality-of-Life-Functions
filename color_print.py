c0b4e979c7eca5d3ca080f5233537419acd3d35c
Lapis <88260244+LapisPhoenix@users.noreply.github.com>
2023-01-20 18:27:50 -0800

    Update color_print.py

--- color_print.py
+++ color_print.py
@@ -4,28 +4,29 @@ import colorama
 def colorp(level: int, message: str, end: str = '\n'):
     """
     Prints Colored Text
-
     Arguments:
         level:   int = The message level.
         message: str = The message to display.
-        end:     str = The end if the message. (DEFAULT: '\n')
-
+        end:     str = The end if the message. (DEFAULT: '\\n')
     Level System:
-        1 - Info
-        2 - Warning
-        3 - Error
-        4 - Critical Error
+        0 - Success\n
+        1 - Info\n
+        2 - Warning\n
+        3 - Error\n
+        4 - Critical Error\n
     """
 
-    if level not in range(1, 5):
+    if level not in range(0, 5):
         raise Exception(f"Invalid level {level}. Level has to be between 1-4")
 
-    level_colors = {1: colorama.Fore.LIGHTYELLOW_EX,
+    level_colors = {0: colorama.Fore.LIGHTGREEN_EX,
+                    1: colorama.Fore.LIGHTYELLOW_EX,
                     2: colorama.Fore.YELLOW,
                     3: colorama.Fore.LIGHTRED_EX,
                     4: colorama.Fore.RED}
 
-    level_types = {1: "INFO",
+    level_types = {0: "SUCCESS",
+                   1: "INFO",
                    2: "WARNING",
                    3: "ERROR",
                    4: "CRITICAL ERROR"}
@@ -37,3 +38,4 @@ def colorp(level: int, message: str, end: str = '\n'):
     type = level_types.get(level)
     reset = colorama.Fore.RESET
     print(f"[{color}{type}{reset}] {message}{reset}", end=end)
+    return f"[{color}{type}{reset}] {message}{reset}{end}"
