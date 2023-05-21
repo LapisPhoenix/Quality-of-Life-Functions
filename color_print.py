5da25f9876979f2560ab3126dc95408584fade1e
Lapis <88260244+LapisPhoenix@users.noreply.github.com>
2023-05-21 16:15:17 -0700

    ✨Better Compatibility && Easier use

--- color_print.py
+++ color_print.py
@@ -1,44 +1,75 @@
+import sys
 import colorama
+from enum import Enum
 
 
-def colorp(level: int, message: str, end: str = '\n'):
-    """
-    Prints colored text.
-    Arguments:
-        level:   int = The message level (0-4).
-        message: str = The message to display.
-        end:     str = The end of the message (default: '\n').
-    Level system:
-        0 - Success\n
-        1 - Info\n
-        2 - Warning\n
-        3 - Error\n
-        4 - Critical Error\n
-    """
+class Logger:
 
-    level_colors = {
-        0: colorama.Fore.LIGHTGREEN_EX,   # Success
-        1: colorama.Fore.LIGHTYELLOW_EX,  # Info
-        2: colorama.Fore.YELLOW,          # Warning
-        3: colorama.Fore.LIGHTRED_EX,     # Error
-        4: colorama.Fore.RED              # Critical Error
-    }
+    class Level(Enum):
+        SUCCESS = 0
+        INFO = 1
+        WARNING = 2
+        ERROR = 3
+        CRITICAL_ERROR = 4
 
-    level_types = {
-        0: "SUCCESS",
-        1: "INFO",
-        2: "WARNING",
-        3: "ERROR",
-        4: "CRITICAL ERROR"
-    }
+    def colorp(self, level: int, message: str, end: str = '\n'):
+        """
+        # Prints color logs to the console.
 
-    if level not in level_colors:
-        raise ValueError(f"Invalid level {level}. Level must be between 0-4")
+        ## Parameters
+        ---
+        - level: int
+            - The level of the log.
+        - message: str
+            - The message to be printed.
+        - end: str
+            - The end character to be used when printing the message.
+        
+        ## Returns
+        ---
+        - None
 
-    color = level_colors[level]
-    type_ = level_types[level]
-    reset = colorama.Fore.RESET
-    print(f"[{color}{type_}{reset}] {message}{reset}", end=end)
+        ## Raises
+        ---
+        - ValueError
+            - If the level is not between 0-4.
+        
+        ## Example
+        ---
+        ```py
+        from utils.logging import Logger
 
-    if level == 4:
-        exit(message)
+        logger = Logger()
+
+        logger.colorp(Logger.Level.SUCCESS, "This is a success message.")
+        ```
+        """
+
+        colorama.init()
+
+        level_colors = {
+            0: colorama.Fore.LIGHTGREEN_EX,   # Success
+            1: colorama.Fore.LIGHTYELLOW_EX,  # Info
+            2: colorama.Fore.YELLOW,          # Warning
+            3: colorama.Fore.LIGHTRED_EX,     # Error
+            4: colorama.Fore.RED              # Critical Error
+        }
+
+        level_types = {
+            0: "SUCCESS",
+            1: "INFO",
+            2: "WARNING",
+            3: "ERROR",
+            4: "CRITICAL ERROR"
+        }
+
+        if level not in level_colors:
+            raise ValueError(f"Invalid level {level}. Level must be between 0-4")
+
+        color = level_colors[level]
+        type_ = level_types[level]
+        reset = colorama.Fore.RESET
+        print(f"[{color}{type_}{reset}] {message}{reset}", end=end)
+
+        if level == 4:
+            sys.exit(message)
